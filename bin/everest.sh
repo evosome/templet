74869115e3d3d9b992f32361aa56d1bc73881e96
evosome <alex.evosome@gmail.com>
2023-12-10 16:23:08 +0400

    Added requirements for tornado==4.1
    - Added tornado==4.1 that fits both the version of python 3.9 and the everesent agent library.
    - Removed redudant python installation on everest.sh script.

--- bin/everest.sh
+++ bin/everest.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-curl -O https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tgz
-
-pip3 install tornado
-
 git clone https://gitlab.com/everest/agent.git ~/everest_agent
 
 cd ~/everest_agent
